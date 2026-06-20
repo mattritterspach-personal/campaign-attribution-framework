d7f17c3e39b573d32f57740cd8586bda1750f4dd
Matt Ritterspach <matt.ritterspach@gmail.com>
2026-06-20 06:02:15 -0500

    Update dbt/models/intermediate/int_journey_positions.sql

--- dbt/models/intermediate/int_journey_positions.sql
+++ dbt/models/intermediate/int_journey_positions.sql
@@ -1,2 +1,20 @@
 -- int_journey_positions.sql
-SELECT * FROM {{ ref('stg_touchpoints') }}
+-- Enriches each touchpoint with journey position metadata.
+
+WITH base AS (
+    SELECT * FROM {{ ref('stg_touchpoints') }}
+),
+
+with_position AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY deal_id ORDER BY touchpoint_at ASCP) AS touch_number,
+        COUNT(*) OVER (PARTITION BY deal_id) AS n_touches
+    FROM base
+)
+
+SELECT
+    *,
+    touch_number = 1 AS is_first_touch,
+    touch_number = n_touches AS is_last_touch
+FROM with_position
