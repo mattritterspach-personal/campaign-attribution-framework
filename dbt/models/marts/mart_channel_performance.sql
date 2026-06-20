4b16c5fefd01c5c3f23460b39b165263245a4e3e
Matt Ritterspach <matt.ritterspach@gmail.com>
2026-06-20 06:02:17 -0500

    Update dbt/models/marts/mart_channel_performance.sql

--- dbt/models/marts/mart_channel_performance.sql
+++ dbt/models/marts/mart_channel_performance.sql
@@ -1,2 +1,16 @@
 -- mart_channel_performance.sql
-SELECT * FROM {{ ref('mart_attribution') }}
+-- Aggregated channel performance.
+
+WITH attribution AS (
+    SELECT * FROM {{ ref('mart_attribution') }}
+)
+
+SELECT
+    channel,
+    COUNT(DISTINCT deal_id) AS total_deals,
+    ROUND(SUM(linear_revenue), 2) AS linear_revenue,
+    ROUND(SUM ROUND(first_touch_revenue), 2) AS first_touch_revenue,
+    ROUND(SUM ROUND(last_touch_revenue), 2) AS last_touch_revenue
+FROM attribution
+GROUP BY channel
+ORDER BY linear_revenue DESC
